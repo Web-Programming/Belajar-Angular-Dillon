ff1d222f3568e3317848bc8de9672ea815ff37d4
20040815 <erloydillon_2226240012@mhs.mdp.ac.id>
2024-11-22 18:32:56 +0700

    Latihan Express Dillon

--- belajar_Angular/src/app/details/details.component.ts
+++ belajar_Angular/src/app/details/details.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
      <article >
       <img class="listing-photo" [src]="housingLocation?.photo"
@@ -24,6 +25,20 @@ import { HousingLocation } from '../housing-location';
           <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplyForm()">
+          <label for="first-name">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          
+          <label for="email">Email</label>
+          <input type="email" id="email" formControlName="email" placeholder="Input email">
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
@@ -32,7 +47,7 @@ export class DetailsComponent {
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined
-  applyForm: FormGroup = new formGroup({
+  applyForm: FormGroup = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl('')
@@ -40,18 +55,19 @@ export class DetailsComponent {
 
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
+    this.housingService.getHousingLocationById(this.housingLocationId)
+      .then(location => {
+        this.housingLocation = location;
+      })
     console.table(this.housingLocation)
   }
 
   submitApplyForm(){
-  this.housingService.submitApplication(
-    this.applyForm.value.firstName ?? '',
-    this.applyForm.value.lastName ?? '',
-    this.applyForm.value.email ?? '',
-  )  
-
-    //panggil API simpan data registrasi via service
+    //panggil API simpan data registarsi via service
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
   }
-
 }
